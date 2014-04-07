11ad05091003bc6e78c52733a3dc25e48b407504
Ben Rousch <brousch@gmail.com>
2014-04-07 15:45:07 -0400

    Update buildozer_only.sh

--- ubuntu/1204/python33/buildozer_only.sh
+++ ubuntu/1204/python33/buildozer_only.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 
 # Install necessary system packages
-sudo apt-get install -y build-essential git zlib1g-dev python3.3 python3.3-dev
+sudo apt-get install -y build-essential git zlib1g-dev python3.3 python3.3-dev ia32-libs
 
 # Bootstrap a current Python environment
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python3.3
