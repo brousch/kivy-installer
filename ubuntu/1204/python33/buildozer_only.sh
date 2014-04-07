97b18567f07e1e5b58197f854033a7d6fdb43d80
Ben Rousch <brousch@gmail.com>
2014-04-07 08:31:29 -0400

    Update buildozer_only.sh

--- ubuntu/1204/python33/buildozer_only.sh
+++ ubuntu/1204/python33/buildozer_only.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# Bootstrap Python3.3
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+
+# Install necessary system packages
 sudo apt-get install -y build-essential git zlib1g-dev python3.3 python3.3-dev
+
+# Bootstrap a current Python environment
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python3.3
 rm setuptools*.zip
 sudo easy_install-3.3 -U pip
 sudo pip3.3 install -U virtualenv
+
+# Install current version of Cython
+sudo apt-get remove --purge -y cython
 sudo pip3.3 install -U cython
+
 #sudo pip3.3 install -U git+https://github.com/kivy/buildozer.git@master
