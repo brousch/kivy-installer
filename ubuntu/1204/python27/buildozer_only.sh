7732a3a79927ac5d3d711485a36d7b8d9ed5d8c5
Ben Rousch <brousch@gmail.com>
2014-04-07 08:30:11 -0400

    Update buildozer_only.sh

--- ubuntu/1204/python27/buildozer_only.sh
+++ ubuntu/1204/python27/buildozer_only.sh
@@ -1,9 +1,15 @@
  #!/bin/sh
 
+# Install necessary system packages
 sudo apt-get install -y build-essential git zlib1g-dev python2.7 python2.7-dev
+
+# Bootstrap a current Python environment
+sudo apt-get remove --purge -y python-virtualenv python-pip python-setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python2.7
 rm setuptools*.zip
 sudo easy_install-2.7 -U pip
 sudo pip2.7 install -U virtualenv
+
+# Install current version of Cython
+sudo apt-get remove --purge -y cython
 sudo pip2.7 install -U cython
-sudo pip2.7 install -U git+https://github.com/kivy/buildozer.git@master
