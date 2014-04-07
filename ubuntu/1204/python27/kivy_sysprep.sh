14d29b2dbc3ee4a4f6dc3e1e4ab73dfdca01d906
Ben Rousch <brousch@gmail.com>
2014-04-07 08:22:04 -0400

    Update kivy_sysprep.sh

--- ubuntu/1204/python27/kivy_sysprep.sh
+++ ubuntu/1204/python27/kivy_sysprep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Install necessary system packages
-sudo apt-get install -y build-essential mercurial git python2.7 python-setuptools python-dev ffmpeg libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev openjdk-7-jdk
+sudo apt-get install -y build-essential mercurial git python2.7 python-dev ffmpeg libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev openjdk-7-jdk
 
 # Bootstrap a current Python environment
+sudo apt-get remove --purge -y python-virtualenv python-pip python-setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python2.7
 rm setuptools*.zip
-sudo apt-get remove --purge -y python-virtualenv python-pip
 sudo easy_install-2.7 -U pip
 sudo pip2.7 install -U virtualenv
 
