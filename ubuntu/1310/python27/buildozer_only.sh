5d0114192f55525e5258ad18ab099f24583273dd
Ben Rousch <brousch@gmail.com>
2014-04-21 08:03:32 -0400

    Update buildozer_only.sh

--- ubuntu/1310/python27/buildozer_only.sh
+++ ubuntu/1310/python27/buildozer_only.sh
@@ -3,7 +3,7 @@
 # Install necessary system packages
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install -y build-essential git zlib1g-dev python2.7 python2.7-dev libncurses5:i386 libstdc++6:i386 zlib1g:i386 openjdk-7-jdk unzip
+sudo apt-get install -y build-essential ccache git zlib1g-dev python2.7 python2.7-dev libncurses5:i386 libstdc++6:i386 zlib1g:i386 openjdk-7-jdk unzip
 
 # Bootstrap a current Python environment
 sudo apt-get remove --purge -y python-virtualenv python-pip python-setuptools
