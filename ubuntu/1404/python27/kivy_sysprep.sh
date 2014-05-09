95b4f756425a9b23c2e2883cc164da61911ed65c
Ben Rousch <brousch@gmail.com>
2014-05-09 14:56:49 -0400

    Update kivy_sysprep.sh

--- ubuntu/1404/python27/kivy_sysprep.sh
+++ ubuntu/1404/python27/kivy_sysprep.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y build-essential mercurial git python2.7 python-dev libfr
 # Bootstrap a current Python environment
 sudo apt-get remove --purge -y python-virtualenv python-pip python-setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python2.7
-rm setuptools*.zip
+rm -f setuptools*.zip
 sudo easy_install-2.7 -U pip
 sudo pip2.7 install -U virtualenv
 
