012748f726fd8c069e2e2f09cf6be165835a170c
Ben Rousch <brousch@gmail.com>
2014-05-23 09:16:21 -0400

    Removed virtualenv from system install

--- ubuntu/1404/python27/kivy_all.sh
+++ ubuntu/1404/python27/kivy_all.sh
@@ -3,11 +3,10 @@
 sudo apt-get install -y build-essential mercurial git python2.7 python-dev libfreetype6-dev libjpeg-dev libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev zlib1g-dev unzip
 
 # Bootstrap a current Python environment
-sudo apt-get remove --purge -y python-virtualenv python-pip python-setuptools
+sudo apt-get remove --purge -y python-pip python-setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python2.7
 rm -f setuptools*.zip
 sudo easy_install-2.7 -U pip
-sudo pip2.7 install -U virtualenv
 
 # Install current version of Cython
 sudo apt-get remove --purge -y cython
